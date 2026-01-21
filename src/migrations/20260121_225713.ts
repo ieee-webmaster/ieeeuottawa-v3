46a8a43efd31f4da6f369c1313d3a23f192009c2
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-01-21 18:00:42 -0500

    chore: fix db state

--- src/migrations/20260121_225713.ts
+++ src/migrations/20260121_225713.ts
@@ -2,20 +2,11 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postg
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TYPE "public"."enum_events_status" AS ENUM('draft', 'published');
+   CREATE TYPE "public"."enum_events_hosted_by" AS ENUM('ieee', 'other');
+  CREATE TYPE "public"."enum_events_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum__events_v_version_hosted_by" AS ENUM('ieee', 'other');
   CREATE TYPE "public"."enum__events_v_version_status" AS ENUM('draft', 'published');
-  CREATE TYPE "public"."enum_committee_ieee_positions_role" AS ENUM('exec', 'commish', 'coord');
-  CREATE TYPE "public"."enum_committee_ieee_positions_position_title" AS ENUM('chair', 'cochair', 'vicechair', 'treasurer', 'mcndirector', 'secretary', 'external', 'academic', 'social', 'equity', 'merch', 'comms', 'webmaster');
-  CREATE TYPE "public"."enum_committee_ieee_positions_commissioner_title" AS ENUM('ieee-elg-commish', 'ieee-ceg-commish', 'ieee-seg-commish', 'ieee-mdsc-commish', 'ieee-comptech-commish', 'ieee-design-commish', 'ieee-translations-commish');
-  CREATE TYPE "public"."enum_committee_ieee_positions_coordinator_title" AS ENUM('ieee-first-year-coord', 'ieee-media-coord', 'ieee-industry-coord', 'ieee-tech-coord', 'ieee-sw-tech-coord');
-  CREATE TYPE "public"."enum_committee_mdsc_positions_role" AS ENUM('exec', 'commish', 'coord');
-  CREATE TYPE "public"."enum_committee_mdsc_positions_position_title" AS ENUM('mdsc-chair', 'mdsc-cochair', 'mdsc-vicechair', 'mdsc-events', 'mdsc-comms');
-  CREATE TYPE "public"."enum_committee_mdsc_positions_commissioner_title" AS ENUM('mdsc-pm', 'mdsc-epp', 'mdsc-swd', 'mdsc-sustain', 'mdsc-first-year-rep');
-  CREATE TYPE "public"."enum_committee_wie_positions_role" AS ENUM('exec', 'commish', 'coord');
-  CREATE TYPE "public"."enum_committee_wie_positions_position_title" AS ENUM('wie-chair', 'wie-cochair', 'wie-vicechair', 'wie-finance', 'wie-external', 'wie-internal');
-  CREATE TYPE "public"."enum_committee_wie_positions_commissioner_title" AS ENUM('wie-design-commish');
-  CREATE TYPE "public"."enum_committee_cegsc_positions_role" AS ENUM('exec', 'commish', 'coord');
-  CREATE TYPE "public"."enum_committee_cegsc_positions_position_title" AS ENUM('cegsc-chair', 'cegsc-cochair', 'cegsc-vicechair', 'cegsc-events', 'cegsc-comms');
+  CREATE TYPE "public"."enum_teams_positions_role" AS ENUM('exec', 'commish', 'coord');
   ALTER TYPE "public"."enum_payload_folders_folder_type" ADD VALUE 'events';
   ALTER TYPE "public"."enum_payload_folders_folder_type" ADD VALUE 'people';
   CREATE TABLE "events" (
@@ -23,7 +14,7 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"title" varchar,
   	"date" timestamp(3) with time zone,
   	"location" varchar,
-  	"hosted_by" varchar,
+  	"hosted_by" "enum_events_hosted_by",
   	"signup_link" varchar,
   	"hero_image_id" integer,
   	"content" jsonb,
@@ -44,7 +35,7 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"version_title" varchar,
   	"version_date" timestamp(3) with time zone,
   	"version_location" varchar,
-  	"version_hosted_by" varchar,
+  	"version_hosted_by" "enum__events_v_version_hosted_by",
   	"version_signup_link" varchar,
   	"version_hero_image_id" integer,
   	"version_content" jsonb,
@@ -74,40 +65,35 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
-  CREATE TABLE "committee_ieee_positions" (
+  CREATE TABLE "teams_positions" (
   	"_order" integer NOT NULL,
   	"_parent_id" integer NOT NULL,
   	"id" varchar PRIMARY KEY NOT NULL,
-  	"role" "enum_committee_ieee_positions_role" NOT NULL,
-  	"position_title" "enum_committee_ieee_positions_position_title",
-  	"commissioner_title" "enum_committee_ieee_positions_commissioner_title",
-  	"coordinator_title" "enum_committee_ieee_positions_coordinator_title"
+  	"role" "enum_teams_positions_role" NOT NULL,
+  	"position_title" varchar NOT NULL
   );
   
-  CREATE TABLE "committee_mdsc_positions" (
-  	"_order" integer NOT NULL,
-  	"_parent_id" integer NOT NULL,
-  	"id" varchar PRIMARY KEY NOT NULL,
-  	"role" "enum_committee_mdsc_positions_role" NOT NULL,
-  	"position_title" "enum_committee_mdsc_positions_position_title",
-  	"commissioner_title" "enum_committee_mdsc_positions_commissioner_title"
+  CREATE TABLE "teams" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"cover_image_id" integer,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
-  CREATE TABLE "committee_wie_positions" (
+  CREATE TABLE "committee_teams_members" (
   	"_order" integer NOT NULL,
-  	"_parent_id" integer NOT NULL,
+  	"_parent_id" varchar NOT NULL,
   	"id" varchar PRIMARY KEY NOT NULL,
-  	"role" "enum_committee_wie_positions_role" NOT NULL,
-  	"position_title" "enum_committee_wie_positions_position_title",
-  	"commissioner_title" "enum_committee_wie_positions_commissioner_title"
+  	"role" varchar NOT NULL,
+  	"person_id" integer NOT NULL
   );
   
-  CREATE TABLE "committee_cegsc_positions" (
+  CREATE TABLE "committee_teams" (
   	"_order" integer NOT NULL,
   	"_parent_id" integer NOT NULL,
   	"id" varchar PRIMARY KEY NOT NULL,
-  	"role" "enum_committee_cegsc_positions_role" NOT NULL,
-  	"position_title" "enum_committee_cegsc_positions_position_title"
+  	"team_id" integer NOT NULL
   );
   
   CREATE TABLE "committee" (
@@ -115,14 +101,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"year" varchar NOT NULL,
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
-  );
-  
-  CREATE TABLE "committee_rels" (
-  	"id" serial PRIMARY KEY NOT NULL,
-  	"order" integer,
-  	"parent_id" integer NOT NULL,
-  	"path" varchar NOT NULL,
-  	"people_id" integer
   );
   
   CREATE TABLE "docs_general_documents" (
@@ -162,6 +140,7 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   
   ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "events_id" integer;
   ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "people_id" integer;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "teams_id" integer;
   ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "committee_id" integer;
   ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "docs_id" integer;
   ALTER TABLE "events" ADD CONSTRAINT "events_hero_image_id_media_id_fk" FOREIGN KEY ("hero_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
@@ -173,12 +152,12 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   ALTER TABLE "_events_v" ADD CONSTRAINT "_events_v_version_folder_id_payload_folders_id_fk" FOREIGN KEY ("version_folder_id") REFERENCES "public"."payload_folders"("id") ON DELETE set null ON UPDATE no action;
   ALTER TABLE "people" ADD CONSTRAINT "people_headshot_id_media_id_fk" FOREIGN KEY ("headshot_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
   ALTER TABLE "people" ADD CONSTRAINT "people_folder_id_payload_folders_id_fk" FOREIGN KEY ("folder_id") REFERENCES "public"."payload_folders"("id") ON DELETE set null ON UPDATE no action;
-  ALTER TABLE "committee_ieee_positions" ADD CONSTRAINT "committee_ieee_positions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
-  ALTER TABLE "committee_mdsc_positions" ADD CONSTRAINT "committee_mdsc_positions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
-  ALTER TABLE "committee_wie_positions" ADD CONSTRAINT "committee_wie_positions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
-  ALTER TABLE "committee_cegsc_positions" ADD CONSTRAINT "committee_cegsc_positions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
-  ALTER TABLE "committee_rels" ADD CONSTRAINT "committee_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
-  ALTER TABLE "committee_rels" ADD CONSTRAINT "committee_rels_people_fk" FOREIGN KEY ("people_id") REFERENCES "public"."people"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "teams_positions" ADD CONSTRAINT "teams_positions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "teams" ADD CONSTRAINT "teams_cover_image_id_media_id_fk" FOREIGN KEY ("cover_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "committee_teams_members" ADD CONSTRAINT "committee_teams_members_person_id_people_id_fk" FOREIGN KEY ("person_id") REFERENCES "public"."people"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "committee_teams_members" ADD CONSTRAINT "committee_teams_members_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee_teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "committee_teams" ADD CONSTRAINT "committee_teams_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "committee_teams" ADD CONSTRAINT "committee_teams_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "docs_general_documents" ADD CONSTRAINT "docs_general_documents_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "docs_meeting_minutes" ADD CONSTRAINT "docs_meeting_minutes_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "docs_other_documents" ADD CONSTRAINT "docs_other_documents_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
@@ -205,20 +184,20 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX "people_folder_idx" ON "people" USING btree ("folder_id");
   CREATE INDEX "people_updated_at_idx" ON "people" USING btree ("updated_at");
   CREATE INDEX "people_created_at_idx" ON "people" USING btree ("created_at");
-  CREATE INDEX "committee_ieee_positions_order_idx" ON "committee_ieee_positions" USING btree ("_order");
-  CREATE INDEX "committee_ieee_positions_parent_id_idx" ON "committee_ieee_positions" USING btree ("_parent_id");
-  CREATE INDEX "committee_mdsc_positions_order_idx" ON "committee_mdsc_positions" USING btree ("_order");
-  CREATE INDEX "committee_mdsc_positions_parent_id_idx" ON "committee_mdsc_positions" USING btree ("_parent_id");
-  CREATE INDEX "committee_wie_positions_order_idx" ON "committee_wie_positions" USING btree ("_order");
-  CREATE INDEX "committee_wie_positions_parent_id_idx" ON "committee_wie_positions" USING btree ("_parent_id");
-  CREATE INDEX "committee_cegsc_positions_order_idx" ON "committee_cegsc_positions" USING btree ("_order");
-  CREATE INDEX "committee_cegsc_positions_parent_id_idx" ON "committee_cegsc_positions" USING btree ("_parent_id");
+  CREATE INDEX "teams_positions_order_idx" ON "teams_positions" USING btree ("_order");
+  CREATE INDEX "teams_positions_parent_id_idx" ON "teams_positions" USING btree ("_parent_id");
+  CREATE UNIQUE INDEX "teams_name_idx" ON "teams" USING btree ("name");
+  CREATE INDEX "teams_cover_image_idx" ON "teams" USING btree ("cover_image_id");
+  CREATE INDEX "teams_updated_at_idx" ON "teams" USING btree ("updated_at");
+  CREATE INDEX "teams_created_at_idx" ON "teams" USING btree ("created_at");
+  CREATE INDEX "committee_teams_members_order_idx" ON "committee_teams_members" USING btree ("_order");
+  CREATE INDEX "committee_teams_members_parent_id_idx" ON "committee_teams_members" USING btree ("_parent_id");
+  CREATE INDEX "committee_teams_members_person_idx" ON "committee_teams_members" USING btree ("person_id");
+  CREATE INDEX "committee_teams_order_idx" ON "committee_teams" USING btree ("_order");
+  CREATE INDEX "committee_teams_parent_id_idx" ON "committee_teams" USING btree ("_parent_id");
+  CREATE INDEX "committee_teams_team_idx" ON "committee_teams" USING btree ("team_id");
   CREATE INDEX "committee_updated_at_idx" ON "committee" USING btree ("updated_at");
   CREATE INDEX "committee_created_at_idx" ON "committee" USING btree ("created_at");
-  CREATE INDEX "committee_rels_order_idx" ON "committee_rels" USING btree ("order");
-  CREATE INDEX "committee_rels_parent_idx" ON "committee_rels" USING btree ("parent_id");
-  CREATE INDEX "committee_rels_path_idx" ON "committee_rels" USING btree ("path");
-  CREATE INDEX "committee_rels_people_id_idx" ON "committee_rels" USING btree ("people_id");
   CREATE INDEX "docs_general_documents_order_idx" ON "docs_general_documents" USING btree ("_order");
   CREATE INDEX "docs_general_documents_parent_id_idx" ON "docs_general_documents" USING btree ("_parent_id");
   CREATE INDEX "docs_meeting_minutes_order_idx" ON "docs_meeting_minutes" USING btree ("_order");
@@ -230,10 +209,12 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX "docs_created_at_idx" ON "docs" USING btree ("created_at");
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_events_fk" FOREIGN KEY ("events_id") REFERENCES "public"."events"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_people_fk" FOREIGN KEY ("people_id") REFERENCES "public"."people"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_teams_fk" FOREIGN KEY ("teams_id") REFERENCES "public"."teams"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_committee_fk" FOREIGN KEY ("committee_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_docs_fk" FOREIGN KEY ("docs_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
   CREATE INDEX "payload_locked_documents_rels_events_id_idx" ON "payload_locked_documents_rels" USING btree ("events_id");
   CREATE INDEX "payload_locked_documents_rels_people_id_idx" ON "payload_locked_documents_rels" USING btree ("people_id");
+  CREATE INDEX "payload_locked_documents_rels_teams_id_idx" ON "payload_locked_documents_rels" USING btree ("teams_id");
   CREATE INDEX "payload_locked_documents_rels_committee_id_idx" ON "payload_locked_documents_rels" USING btree ("committee_id");
   CREATE INDEX "payload_locked_documents_rels_docs_id_idx" ON "payload_locked_documents_rels" USING btree ("docs_id");`)
 }
@@ -243,12 +224,11 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
    ALTER TABLE "events" DISABLE ROW LEVEL SECURITY;
   ALTER TABLE "_events_v" DISABLE ROW LEVEL SECURITY;
   ALTER TABLE "people" DISABLE ROW LEVEL SECURITY;
-  ALTER TABLE "committee_ieee_positions" DISABLE ROW LEVEL SECURITY;
-  ALTER TABLE "committee_mdsc_positions" DISABLE ROW LEVEL SECURITY;
-  ALTER TABLE "committee_wie_positions" DISABLE ROW LEVEL SECURITY;
-  ALTER TABLE "committee_cegsc_positions" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "teams_positions" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "teams" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "committee_teams_members" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "committee_teams" DISABLE ROW LEVEL SECURITY;
   ALTER TABLE "committee" DISABLE ROW LEVEL SECURITY;
-  ALTER TABLE "committee_rels" DISABLE ROW LEVEL SECURITY;
   ALTER TABLE "docs_general_documents" DISABLE ROW LEVEL SECURITY;
   ALTER TABLE "docs_meeting_minutes" DISABLE ROW LEVEL SECURITY;
   ALTER TABLE "docs_other_documents" DISABLE ROW LEVEL SECURITY;
@@ -256,12 +236,11 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   DROP TABLE "events" CASCADE;
   DROP TABLE "_events_v" CASCADE;
   DROP TABLE "people" CASCADE;
-  DROP TABLE "committee_ieee_positions" CASCADE;
-  DROP TABLE "committee_mdsc_positions" CASCADE;
-  DROP TABLE "committee_wie_positions" CASCADE;
-  DROP TABLE "committee_cegsc_positions" CASCADE;
+  DROP TABLE "teams_positions" CASCADE;
+  DROP TABLE "teams" CASCADE;
+  DROP TABLE "committee_teams_members" CASCADE;
+  DROP TABLE "committee_teams" CASCADE;
   DROP TABLE "committee" CASCADE;
-  DROP TABLE "committee_rels" CASCADE;
   DROP TABLE "docs_general_documents" CASCADE;
   DROP TABLE "docs_meeting_minutes" CASCADE;
   DROP TABLE "docs_other_documents" CASCADE;
@@ -269,6 +248,8 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_events_fk";
   
   ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_people_fk";
+  
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_teams_fk";
   
   ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_committee_fk";
   
@@ -280,24 +261,17 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   ALTER TABLE "payload_folders_folder_type" ALTER COLUMN "value" SET DATA TYPE "public"."enum_payload_folders_folder_type" USING "value"::"public"."enum_payload_folders_folder_type";
   DROP INDEX "payload_locked_documents_rels_events_id_idx";
   DROP INDEX "payload_locked_documents_rels_people_id_idx";
+  DROP INDEX "payload_locked_documents_rels_teams_id_idx";
   DROP INDEX "payload_locked_documents_rels_committee_id_idx";
   DROP INDEX "payload_locked_documents_rels_docs_id_idx";
   ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "events_id";
   ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "people_id";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "teams_id";
   ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "committee_id";
   ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "docs_id";
+  DROP TYPE "public"."enum_events_hosted_by";
   DROP TYPE "public"."enum_events_status";
+  DROP TYPE "public"."enum__events_v_version_hosted_by";
   DROP TYPE "public"."enum__events_v_version_status";
-  DROP TYPE "public"."enum_committee_ieee_positions_role";
-  DROP TYPE "public"."enum_committee_ieee_positions_position_title";
-  DROP TYPE "public"."enum_committee_ieee_positions_commissioner_title";
-  DROP TYPE "public"."enum_committee_ieee_positions_coordinator_title";
-  DROP TYPE "public"."enum_committee_mdsc_positions_role";
-  DROP TYPE "public"."enum_committee_mdsc_positions_position_title";
-  DROP TYPE "public"."enum_committee_mdsc_positions_commissioner_title";
-  DROP TYPE "public"."enum_committee_wie_positions_role";
-  DROP TYPE "public"."enum_committee_wie_positions_position_title";
-  DROP TYPE "public"."enum_committee_wie_positions_commissioner_title";
-  DROP TYPE "public"."enum_committee_cegsc_positions_role";
-  DROP TYPE "public"."enum_committee_cegsc_positions_position_title";`)
+  DROP TYPE "public"."enum_teams_positions_role";`)
 }
