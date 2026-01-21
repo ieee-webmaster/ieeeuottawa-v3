4dfba3094002852fe549c0e73c362942f72b7846
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-01-21 15:56:03 -0500

    feat: new schema & migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,5 @@
 import * as migration_20260105_201500_initial from './20260105_201500_initial';
-import * as migration_20260116_183904 from './20260116_183904';
-import * as migration_20260121_173752 from './20260121_173752';
+import * as migration_20260121_205528 from './20260121_205528';
 
 export const migrations = [
   {
@@ -9,13 +8,8 @@ export const migrations = [
     name: '20260105_201500_initial',
   },
   {
-    up: migration_20260116_183904.up,
-    down: migration_20260116_183904.down,
-    name: '20260116_183904',
-  },
-  {
-    up: migration_20260121_173752.up,
-    down: migration_20260121_173752.down,
-    name: '20260121_173752'
+    up: migration_20260121_205528.up,
+    down: migration_20260121_205528.down,
+    name: '20260121_205528'
   },
 ];
