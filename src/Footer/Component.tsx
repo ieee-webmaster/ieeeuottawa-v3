561325f5788e068ef16b9c44775d3e8c63cf1dd7
aparnabalaji07 <aparna.balaji@gmail.com>
2026-02-03 13:54:23 -0500

    feat: getFullYear for copyright, full footer finished

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -13,27 +13,34 @@ export async function Footer() {
 
   const navItems = footerData?.navItems || []
 
+  let currentYear = new Date().getFullYear()
+
   return (
     <footer className="mt-auto border-t border-primary bg-primary text-primary-foreground">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
-          <Logo />
-        </Link>
+  <div className="container py-8 flex flex-col gap-6">
+    
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <ThemeSelector />
-          <nav className="flex flex-col md:flex-row gap-4">
-            {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
-            })}
+    <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4">
+      <Link className="flex items-center" href="/">
+        <Logo />
+      </Link>
 
-            <a href= "https://www.instagram.com/ieeeuottawa/" className = "text-white">Instagram</a>
-            <a href= "https://www.linkedin.com/company/ieeeuottawa/posts/?feedView=all" className = "text-white">LinkedIn</a>
-            <a href="https://www.youtube.com/channel/UCSv1Vna97rKa8w2ktRG8wRw/videos" className = "text-white">YouTube</a>
-            <a href="https://discord.gg/kyTRZ6Ke6J" className = "text-white">Discord</a>
-          </nav>
-        </div>
+      <div className="flex flex-col-reverse md:flex-row gap-4 md:items-center">
+        <ThemeSelector />
+        <nav className="flex gap-4">
+          {navItems.map(({ link }, i) => (
+            <CMSLink className="text-white" key={i} {...link} />
+          ))}
+        </nav>
       </div>
-    </footer>
+    </div>
+
+
+    <p className="text-xs text-white/70 text-center md:text-left">
+      &copy; {currentYear} IEEE UOttawa. All rights reserved.
+    </p>
+
+    </div>
+  </footer>
   )
 }
