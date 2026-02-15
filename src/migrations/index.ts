9c02f3c35828873542d8b456e3b95067f2d7da9d
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-02-15 12:08:32 -0500

    fix: migrations problem

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,10 +1,4 @@
-import * as migration_20260105_201500_initial from './20260105_201500_initial';
-import * as migration_20260121_225713 from './20260121_225713';
-import * as migration_20260121_231101 from './20260121_231101';
-import * as migration_20260206_154616 from './20260206_154616';
-import * as migration_20260206_163630 from './20260206_163630';
-import * as migration_20260212_170848 from './20260212_170848';
-import * as migration_20260215_165316 from './20260215_165316';
+import * as migration_20260105_201500_initial from './20260105_201500_initial'
 
 export const migrations = [
   {
@@ -12,34 +6,4 @@ export const migrations = [
     down: migration_20260105_201500_initial.down,
     name: '20260105_201500_initial',
   },
-  {
-    up: migration_20260121_225713.up,
-    down: migration_20260121_225713.down,
-    name: '20260121_225713',
-  },
-  {
-    up: migration_20260121_231101.up,
-    down: migration_20260121_231101.down,
-    name: '20260121_231101',
-  },
-  {
-    up: migration_20260206_154616.up,
-    down: migration_20260206_154616.down,
-    name: '20260206_154616',
-  },
-  {
-    up: migration_20260206_163630.up,
-    down: migration_20260206_163630.down,
-    name: '20260206_163630',
-  },
-  {
-    up: migration_20260212_170848.up,
-    down: migration_20260212_170848.down,
-    name: '20260212_170848',
-  },
-  {
-    up: migration_20260215_165316.up,
-    down: migration_20260215_165316.down,
-    name: '20260215_165316'
-  },
-];
+]
