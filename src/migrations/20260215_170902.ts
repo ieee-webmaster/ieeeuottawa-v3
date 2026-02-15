deb0e8725eb8e543fd5a12b5d372d444f9f4d961
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-02-15 12:09:13 -0500

    fix: add combined migrations

---
+++ src/migrations/20260215_170902.ts
@@ -0,0 +1,306 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TYPE "public"."enum_events_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum__events_v_version_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum_teams_positions_role" AS ENUM('exec', 'commish', 'coord');
+  ALTER TYPE "public"."enum_payload_folders_folder_type" ADD VALUE 'events';
+  ALTER TYPE "public"."enum_payload_folders_folder_type" ADD VALUE 'people';
+  CREATE TABLE "events" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"title" varchar,
+  	"date" timestamp(3) with time zone,
+  	"location" varchar,
+  	"signup_link" varchar,
+  	"media_link" varchar,
+  	"hero_image_id" integer,
+  	"content" jsonb,
+  	"meta_title" varchar,
+  	"meta_image_id" integer,
+  	"meta_description" varchar,
+  	"generate_slug" boolean DEFAULT true,
+  	"slug" varchar,
+  	"folder_id" integer,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"_status" "enum_events_status" DEFAULT 'draft'
+  );
+  
+  CREATE TABLE "events_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"teams_id" integer
+  );
+  
+  CREATE TABLE "_events_v" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"parent_id" integer,
+  	"version_title" varchar,
+  	"version_date" timestamp(3) with time zone,
+  	"version_location" varchar,
+  	"version_signup_link" varchar,
+  	"version_media_link" varchar,
+  	"version_hero_image_id" integer,
+  	"version_content" jsonb,
+  	"version_meta_title" varchar,
+  	"version_meta_image_id" integer,
+  	"version_meta_description" varchar,
+  	"version_generate_slug" boolean DEFAULT true,
+  	"version_slug" varchar,
+  	"version_folder_id" integer,
+  	"version_updated_at" timestamp(3) with time zone,
+  	"version_created_at" timestamp(3) with time zone,
+  	"version__status" "enum__events_v_version_status" DEFAULT 'draft',
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"latest" boolean,
+  	"autosave" boolean
+  );
+  
+  CREATE TABLE "_events_v_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"teams_id" integer
+  );
+  
+  CREATE TABLE "people" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"full_name" varchar NOT NULL,
+  	"headshot_id" integer,
+  	"contact_email" varchar,
+  	"linkedin_profile" varchar,
+  	"folder_id" integer,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "teams_positions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"role" "enum_teams_positions_role" NOT NULL,
+  	"position_title" varchar NOT NULL
+  );
+  
+  CREATE TABLE "teams" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"cover_image_id" integer,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "committee_teams_members" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" varchar NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"role" varchar NOT NULL,
+  	"person_id" integer NOT NULL
+  );
+  
+  CREATE TABLE "committee_teams" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"team_id" integer NOT NULL
+  );
+  
+  CREATE TABLE "committee" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"year" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "docs_general_documents" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"description" varchar,
+  	"google_docs_url" varchar NOT NULL
+  );
+  
+  CREATE TABLE "docs_meeting_minutes" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"meeting_date" timestamp(3) with time zone,
+  	"description" varchar,
+  	"google_docs_url" varchar NOT NULL
+  );
+  
+  CREATE TABLE "docs_other_documents" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"description" varchar,
+  	"google_docs_url" varchar NOT NULL
+  );
+  
+  CREATE TABLE "docs" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"year" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "events_id" integer;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "people_id" integer;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "teams_id" integer;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "committee_id" integer;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "docs_id" integer;
+  ALTER TABLE "events" ADD CONSTRAINT "events_hero_image_id_media_id_fk" FOREIGN KEY ("hero_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "events" ADD CONSTRAINT "events_meta_image_id_media_id_fk" FOREIGN KEY ("meta_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "events" ADD CONSTRAINT "events_folder_id_payload_folders_id_fk" FOREIGN KEY ("folder_id") REFERENCES "public"."payload_folders"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "events_rels" ADD CONSTRAINT "events_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."events"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "events_rels" ADD CONSTRAINT "events_rels_teams_fk" FOREIGN KEY ("teams_id") REFERENCES "public"."teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_events_v" ADD CONSTRAINT "_events_v_parent_id_events_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."events"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_events_v" ADD CONSTRAINT "_events_v_version_hero_image_id_media_id_fk" FOREIGN KEY ("version_hero_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_events_v" ADD CONSTRAINT "_events_v_version_meta_image_id_media_id_fk" FOREIGN KEY ("version_meta_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_events_v" ADD CONSTRAINT "_events_v_version_folder_id_payload_folders_id_fk" FOREIGN KEY ("version_folder_id") REFERENCES "public"."payload_folders"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_events_v_rels" ADD CONSTRAINT "_events_v_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."_events_v"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_events_v_rels" ADD CONSTRAINT "_events_v_rels_teams_fk" FOREIGN KEY ("teams_id") REFERENCES "public"."teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "people" ADD CONSTRAINT "people_headshot_id_media_id_fk" FOREIGN KEY ("headshot_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "people" ADD CONSTRAINT "people_folder_id_payload_folders_id_fk" FOREIGN KEY ("folder_id") REFERENCES "public"."payload_folders"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "teams_positions" ADD CONSTRAINT "teams_positions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "teams" ADD CONSTRAINT "teams_cover_image_id_media_id_fk" FOREIGN KEY ("cover_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "committee_teams_members" ADD CONSTRAINT "committee_teams_members_person_id_people_id_fk" FOREIGN KEY ("person_id") REFERENCES "public"."people"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "committee_teams_members" ADD CONSTRAINT "committee_teams_members_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee_teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "committee_teams" ADD CONSTRAINT "committee_teams_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "committee_teams" ADD CONSTRAINT "committee_teams_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "docs_general_documents" ADD CONSTRAINT "docs_general_documents_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "docs_meeting_minutes" ADD CONSTRAINT "docs_meeting_minutes_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "docs_other_documents" ADD CONSTRAINT "docs_other_documents_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "events_hero_image_idx" ON "events" USING btree ("hero_image_id");
+  CREATE INDEX "events_meta_meta_image_idx" ON "events" USING btree ("meta_image_id");
+  CREATE UNIQUE INDEX "events_slug_idx" ON "events" USING btree ("slug");
+  CREATE INDEX "events_folder_idx" ON "events" USING btree ("folder_id");
+  CREATE INDEX "events_updated_at_idx" ON "events" USING btree ("updated_at");
+  CREATE INDEX "events_created_at_idx" ON "events" USING btree ("created_at");
+  CREATE INDEX "events__status_idx" ON "events" USING btree ("_status");
+  CREATE INDEX "events_rels_order_idx" ON "events_rels" USING btree ("order");
+  CREATE INDEX "events_rels_parent_idx" ON "events_rels" USING btree ("parent_id");
+  CREATE INDEX "events_rels_path_idx" ON "events_rels" USING btree ("path");
+  CREATE INDEX "events_rels_teams_id_idx" ON "events_rels" USING btree ("teams_id");
+  CREATE INDEX "_events_v_parent_idx" ON "_events_v" USING btree ("parent_id");
+  CREATE INDEX "_events_v_version_version_hero_image_idx" ON "_events_v" USING btree ("version_hero_image_id");
+  CREATE INDEX "_events_v_version_meta_version_meta_image_idx" ON "_events_v" USING btree ("version_meta_image_id");
+  CREATE INDEX "_events_v_version_version_slug_idx" ON "_events_v" USING btree ("version_slug");
+  CREATE INDEX "_events_v_version_version_folder_idx" ON "_events_v" USING btree ("version_folder_id");
+  CREATE INDEX "_events_v_version_version_updated_at_idx" ON "_events_v" USING btree ("version_updated_at");
+  CREATE INDEX "_events_v_version_version_created_at_idx" ON "_events_v" USING btree ("version_created_at");
+  CREATE INDEX "_events_v_version_version__status_idx" ON "_events_v" USING btree ("version__status");
+  CREATE INDEX "_events_v_created_at_idx" ON "_events_v" USING btree ("created_at");
+  CREATE INDEX "_events_v_updated_at_idx" ON "_events_v" USING btree ("updated_at");
+  CREATE INDEX "_events_v_latest_idx" ON "_events_v" USING btree ("latest");
+  CREATE INDEX "_events_v_autosave_idx" ON "_events_v" USING btree ("autosave");
+  CREATE INDEX "_events_v_rels_order_idx" ON "_events_v_rels" USING btree ("order");
+  CREATE INDEX "_events_v_rels_parent_idx" ON "_events_v_rels" USING btree ("parent_id");
+  CREATE INDEX "_events_v_rels_path_idx" ON "_events_v_rels" USING btree ("path");
+  CREATE INDEX "_events_v_rels_teams_id_idx" ON "_events_v_rels" USING btree ("teams_id");
+  CREATE INDEX "people_headshot_idx" ON "people" USING btree ("headshot_id");
+  CREATE INDEX "people_folder_idx" ON "people" USING btree ("folder_id");
+  CREATE INDEX "people_updated_at_idx" ON "people" USING btree ("updated_at");
+  CREATE INDEX "people_created_at_idx" ON "people" USING btree ("created_at");
+  CREATE INDEX "teams_positions_order_idx" ON "teams_positions" USING btree ("_order");
+  CREATE INDEX "teams_positions_parent_id_idx" ON "teams_positions" USING btree ("_parent_id");
+  CREATE UNIQUE INDEX "teams_name_idx" ON "teams" USING btree ("name");
+  CREATE INDEX "teams_cover_image_idx" ON "teams" USING btree ("cover_image_id");
+  CREATE INDEX "teams_updated_at_idx" ON "teams" USING btree ("updated_at");
+  CREATE INDEX "teams_created_at_idx" ON "teams" USING btree ("created_at");
+  CREATE INDEX "committee_teams_members_order_idx" ON "committee_teams_members" USING btree ("_order");
+  CREATE INDEX "committee_teams_members_parent_id_idx" ON "committee_teams_members" USING btree ("_parent_id");
+  CREATE INDEX "committee_teams_members_person_idx" ON "committee_teams_members" USING btree ("person_id");
+  CREATE INDEX "committee_teams_order_idx" ON "committee_teams" USING btree ("_order");
+  CREATE INDEX "committee_teams_parent_id_idx" ON "committee_teams" USING btree ("_parent_id");
+  CREATE INDEX "committee_teams_team_idx" ON "committee_teams" USING btree ("team_id");
+  CREATE UNIQUE INDEX "committee_year_idx" ON "committee" USING btree ("year");
+  CREATE INDEX "committee_updated_at_idx" ON "committee" USING btree ("updated_at");
+  CREATE INDEX "committee_created_at_idx" ON "committee" USING btree ("created_at");
+  CREATE INDEX "docs_general_documents_order_idx" ON "docs_general_documents" USING btree ("_order");
+  CREATE INDEX "docs_general_documents_parent_id_idx" ON "docs_general_documents" USING btree ("_parent_id");
+  CREATE INDEX "docs_meeting_minutes_order_idx" ON "docs_meeting_minutes" USING btree ("_order");
+  CREATE INDEX "docs_meeting_minutes_parent_id_idx" ON "docs_meeting_minutes" USING btree ("_parent_id");
+  CREATE INDEX "docs_other_documents_order_idx" ON "docs_other_documents" USING btree ("_order");
+  CREATE INDEX "docs_other_documents_parent_id_idx" ON "docs_other_documents" USING btree ("_parent_id");
+  CREATE UNIQUE INDEX "docs_year_idx" ON "docs" USING btree ("year");
+  CREATE INDEX "docs_updated_at_idx" ON "docs" USING btree ("updated_at");
+  CREATE INDEX "docs_created_at_idx" ON "docs" USING btree ("created_at");
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_events_fk" FOREIGN KEY ("events_id") REFERENCES "public"."events"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_people_fk" FOREIGN KEY ("people_id") REFERENCES "public"."people"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_teams_fk" FOREIGN KEY ("teams_id") REFERENCES "public"."teams"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_committee_fk" FOREIGN KEY ("committee_id") REFERENCES "public"."committee"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_docs_fk" FOREIGN KEY ("docs_id") REFERENCES "public"."docs"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "payload_locked_documents_rels_events_id_idx" ON "payload_locked_documents_rels" USING btree ("events_id");
+  CREATE INDEX "payload_locked_documents_rels_people_id_idx" ON "payload_locked_documents_rels" USING btree ("people_id");
+  CREATE INDEX "payload_locked_documents_rels_teams_id_idx" ON "payload_locked_documents_rels" USING btree ("teams_id");
+  CREATE INDEX "payload_locked_documents_rels_committee_id_idx" ON "payload_locked_documents_rels" USING btree ("committee_id");
+  CREATE INDEX "payload_locked_documents_rels_docs_id_idx" ON "payload_locked_documents_rels" USING btree ("docs_id");`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "events" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "events_rels" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "_events_v" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "_events_v_rels" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "people" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "teams_positions" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "teams" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "committee_teams_members" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "committee_teams" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "committee" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "docs_general_documents" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "docs_meeting_minutes" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "docs_other_documents" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "docs" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "events" CASCADE;
+  DROP TABLE "events_rels" CASCADE;
+  DROP TABLE "_events_v" CASCADE;
+  DROP TABLE "_events_v_rels" CASCADE;
+  DROP TABLE "people" CASCADE;
+  DROP TABLE "teams_positions" CASCADE;
+  DROP TABLE "teams" CASCADE;
+  DROP TABLE "committee_teams_members" CASCADE;
+  DROP TABLE "committee_teams" CASCADE;
+  DROP TABLE "committee" CASCADE;
+  DROP TABLE "docs_general_documents" CASCADE;
+  DROP TABLE "docs_meeting_minutes" CASCADE;
+  DROP TABLE "docs_other_documents" CASCADE;
+  DROP TABLE "docs" CASCADE;
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_events_fk";
+  
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_people_fk";
+  
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_teams_fk";
+  
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_committee_fk";
+  
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_docs_fk";
+  
+  ALTER TABLE "payload_folders_folder_type" ALTER COLUMN "value" SET DATA TYPE text;
+  DROP TYPE "public"."enum_payload_folders_folder_type";
+  CREATE TYPE "public"."enum_payload_folders_folder_type" AS ENUM('media');
+  ALTER TABLE "payload_folders_folder_type" ALTER COLUMN "value" SET DATA TYPE "public"."enum_payload_folders_folder_type" USING "value"::"public"."enum_payload_folders_folder_type";
+  DROP INDEX "payload_locked_documents_rels_events_id_idx";
+  DROP INDEX "payload_locked_documents_rels_people_id_idx";
+  DROP INDEX "payload_locked_documents_rels_teams_id_idx";
+  DROP INDEX "payload_locked_documents_rels_committee_id_idx";
+  DROP INDEX "payload_locked_documents_rels_docs_id_idx";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "events_id";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "people_id";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "teams_id";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "committee_id";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "docs_id";
+  DROP TYPE "public"."enum_events_status";
+  DROP TYPE "public"."enum__events_v_version_status";
+  DROP TYPE "public"."enum_teams_positions_role";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20260105_201500_initial from './20260105_201500_initial'
+import * as migration_20260105_201500_initial from './20260105_201500_initial';
+import * as migration_20260215_170902 from './20260215_170902';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20260105_201500_initial.down,
     name: '20260105_201500_initial',
   },
-]
+  {
+    up: migration_20260215_170902.up,
+    down: migration_20260215_170902.down,
+    name: '20260215_170902'
+  },
+];
