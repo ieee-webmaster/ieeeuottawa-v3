84d582ad443961e2488079772028e6fd79169ef7
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-02-15 11:38:24 -0500

    fix: affinity group template base

--- src/heros/RenderHero.tsx
+++ src/heros/RenderHero.tsx
@@ -5,11 +5,13 @@ import type { Page } from '@/payload-types'
 import { HighImpactHero } from '@/heros/HighImpact'
 import { LowImpactHero } from '@/heros/LowImpact'
 import { MediumImpactHero } from '@/heros/MediumImpact'
+import { AffinityGroupHero } from '@/heros/AffinityGroup'
 
 const heroes = {
   highImpact: HighImpactHero,
   lowImpact: LowImpactHero,
   mediumImpact: MediumImpactHero,
+  affinityGroup: AffinityGroupHero,
 }
 
 export const RenderHero: React.FC<Page['hero']> = (props) => {
