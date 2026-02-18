72d2db937c149dda305d2038b2b8dd3e23999e7a
aparnabalaji07 <aparna.balaji@gmail.com>
2026-02-18 11:24:47 -0600

    chore: added and used prettier extension

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -14,34 +14,28 @@ export async function Footer() {
 
   const navItems = footerData?.navItems || []
 
-  
-
   return (
     <footer className="mt-auto border-t border-primary bg-primary text-primary-foreground">
-  <div className="container py-8 flex flex-col gap-6">
-    
+      <div className="container py-8 flex flex-col gap-6">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4">
+          <Link className="flex items-center" href="/">
+            <Logo />
+          </Link>
 
-    <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4">
-      <Link className="flex items-center" href="/">
-        <Logo />
-      </Link>
+          <div className="flex flex-col-reverse md:flex-row gap-4 md:items-center">
+            <ThemeSelector />
+            <nav className="flex gap-4">
+              {navItems.map(({ link }, i) => (
+                <CMSLink className="text-white" key={i} {...link} />
+              ))}
+            </nav>
+          </div>
+        </div>
 
-      <div className="flex flex-col-reverse md:flex-row gap-4 md:items-center">
-        <ThemeSelector />
-        <nav className="flex gap-4">
-          {navItems.map(({ link }, i) => (
-            <CMSLink className="text-white" key={i} {...link} />
-          ))}
-        </nav>
+        <p className="text-xs text-white/70 text-center md:text-left">
+          &copy; {currentYear} IEEE UOttawa. All rights reserved.
+        </p>
       </div>
-    </div>
-
-
-    <p className="text-xs text-white/70 text-center md:text-left">
-      &copy; {currentYear} IEEE UOttawa. All rights reserved.
-    </p>
-
-    </div>
-  </footer>
+    </footer>
   )
 }
