77a8fe3a585787b7ad7cf2827d2d16907c45456e
aparnabalaji07 <aparna.balaji@gmail.com>
2026-02-19 20:31:24 -0600

    chore: added ts types for documents

--- src/app/(frontend)/documents/[slug]/page.tsx
+++ src/app/(frontend)/documents/[slug]/page.tsx
@@ -1,3 +1,17 @@
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
 import { notFound } from 'next/navigation'
+import { Doc } from '@/payload-types'
+
+type DocItem = {
+  name: string
+  description?: string
+  fileUrl: string
+}
+
+type DocsType = {
+  year: string
+  generalDocuments?: DocItem[]
+  meetingMinutes?: (DocItem & { meetingDate?: string })[]
+  otherDocuments?: DocItem[]
+}
