12198eb2f58f2dd168e7db5c69dc02865860931e
Waaberi Ibrahim <wibra007@uottawa.ca>
2026-03-07 17:04:40 -0500

    migrations?

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260105_201500_initial from './20260105_201500_initial';
 import * as migration_20260215_170902 from './20260215_170902';
+import * as migration_20260307_215259_i18n_schema_sync from './20260307_215259_i18n_schema_sync';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260215_170902.up,
     down: migration_20260215_170902.down,
-    name: '20260215_170902'
+    name: '20260215_170902',
+  },
+  {
+    up: migration_20260307_215259_i18n_schema_sync.up,
+    down: migration_20260307_215259_i18n_schema_sync.down,
+    name: '20260307_215259_i18n_schema_sync'
   },
 ];
