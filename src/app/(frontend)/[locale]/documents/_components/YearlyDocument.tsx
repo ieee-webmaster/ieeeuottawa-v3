15e6ce1f2a08d30051f482a2ef898d37c5ae4e32
aparnabalaji07 <aparna.balaji@gmail.com>
2026-04-02 16:05:13 -0400

    fix: added '#'

--- src/app/(frontend)/[locale]/documents/_components/YearlyDocument.tsx
+++ src/app/(frontend)/[locale]/documents/_components/YearlyDocument.tsx
@@ -1,7 +1,7 @@
 import { Doc } from '@/payload-types'
 
 const formatUrl = (url?: string) => {
-  if (!url) return
+  if (!url) return '#'
   if (url.startsWith('http://') || url.startsWith('https://')) return url
   return `https://${url}`
 }
