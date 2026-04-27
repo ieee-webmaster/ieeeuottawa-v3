58afd1b6a236929d56e4f3afd9e0f2ed25265c77
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-04-27 19:09:08 -0400

    chore: create migration for email ownership

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20260427_173319_baseline from './20260427_173319_baseline'
+import * as migration_20260427_173319_baseline from './20260427_173319_baseline';
+import * as migration_20260427_230857 from './20260427_230857';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20260427_173319_baseline.down,
     name: '20260427_173319_baseline',
   },
-]
+  {
+    up: migration_20260427_230857.up,
+    down: migration_20260427_230857.down,
+    name: '20260427_230857'
+  },
+];
