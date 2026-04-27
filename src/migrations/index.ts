0ea3f40297ba811129d1cccb39c4967d4a209bf8
Waaberi Ibrahim <wibra007@uottawa.ca>
2026-04-27 00:58:46 -0400

    chore: create migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20260215_170902 from './20260215_170902';
 import * as migration_20260307_215259_i18n_schema_sync from './20260307_215259_i18n_schema_sync';
 import * as migration_20260402_154329 from './20260402_154329';
 import * as migration_20260415_161459 from './20260415_161459';
+import * as migration_20260427_045818 from './20260427_045818';
 
 export const migrations = [
   {
@@ -28,6 +29,11 @@ export const migrations = [
   {
     up: migration_20260415_161459.up,
     down: migration_20260415_161459.down,
-    name: '20260415_161459'
+    name: '20260415_161459',
+  },
+  {
+    up: migration_20260427_045818.up,
+    down: migration_20260427_045818.down,
+    name: '20260427_045818'
   },
 ];
