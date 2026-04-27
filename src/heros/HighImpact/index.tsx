facd6d050eb57be2e6c4eb25065e17d69a8df87e
Waaberi Ibrahim <wibra007@uottawa.ca>
2026-04-27 00:55:11 -0400

    fix: remove some hardcoded things

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -9,15 +9,18 @@ import RichText from '@/components/RichText'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
-    <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
+    <section
+      className="relative flex min-h-[80vh] items-center justify-center overflow-hidden text-white"
       data-theme="dark"
     >
-      <div className="container mb-8 z-10 relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] md:text-center">
+      {media && typeof media === 'object' && (
+        <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
+      )}
+      <div className="container relative z-10 py-16 md:py-24">
+        <div className="mx-auto max-w-[36.5rem] md:text-center">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex md:justify-center gap-4">
+            <ul className="flex gap-4 md:justify-center">
               {links.map(({ link }, i) => {
                 return (
                   <li key={i}>
@@ -29,11 +32,6 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
           )}
         </div>
       </div>
-      <div className="min-h-[80vh] select-none">
-        {media && typeof media === 'object' && (
-          <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
-        )}
-      </div>
-    </div>
+    </section>
   )
 }
