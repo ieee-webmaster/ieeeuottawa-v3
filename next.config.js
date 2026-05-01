445af82cc7c28f4c31486af248540029d8b6a52f
Waaberi Ibrahim <wibra007@uottawa.ca>
2026-05-01 14:25:25 -0400

    build(compose): Add Vercel Blob Emulator to docker-compose.yml

--- next.config.js
+++ next.config.js
@@ -5,23 +5,28 @@ import redirects from './redirects.js'
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL ||
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000')
+
+const remotePatterns = [NEXT_PUBLIC_SERVER_URL, process.env.STORAGE_VERCEL_BLOB_BASE_URL]
+  .filter(Boolean)
+  .map((item) => {
+    const url = new URL(item)
+
+    return {
+      hostname: url.hostname,
+      port: url.port,
+      protocol: url.protocol.replace(':', ''),
+    }
+  })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
+    remotePatterns,
   },
   reactStrictMode: true,
   redirects,
