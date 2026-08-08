64e77eacb1d9d904e2bd1c5d91e8909845be1b5f
Waaberi Ibrahim <wibra007@uottawa.ca>
2026-08-08 15:58:32 -0400

    revert: reverting commit 03a29f which moved away from vercel blob

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,7 @@ import sharp from 'sharp'
 import path from 'path'
 import { buildConfig, PayloadRequest } from 'payload'
 import { fileURLToPath } from 'url'
-import { s3Storage } from '@payloadcms/storage-s3'
+
 import { Categories } from './collections/Categories'
 import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
@@ -17,6 +17,7 @@ import { People } from './collections/People'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { Committees } from './collections/Committees'
 import { Teams } from './collections/Teams'
 import { Docs } from './collections/Docs'
@@ -92,20 +93,11 @@ export default buildConfig({
   },
   plugins: [
     ...plugins,
-    s3Storage({
+    vercelBlobStorage({
       collections: {
         media: true,
       },
-      bucket: process.env.S3_BUCKET || '',
-      config: {
-        forcePathStyle: true, // Important for using Supabase
-        credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
-        },
-        region: process.env.S3_REGION,
-        endpoint: process.env.S3_ENDPOINT,
-      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
     rbacPlugin({
       collections: [
